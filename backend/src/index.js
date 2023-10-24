b17a04f3e9bf6e6cb2ca06ab77eba28e99f82c71
ErnestDecina <ernestjohn007@gmail.com>
2023-10-24 16:47:59 +0100

    created / route

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World for Node");
+import path from 'path';
+import { express_app } from './server.js';
+
+
+console.log(path.resolve());
+express_app.get('/', (req, res) => {
+    res.sendFile(path.join(path.resolve() + '/../frontend/index.html'));
+});
+
+express_app.listen(8000, () => {
+    console.log('http://localhost:8000/');    
+})
