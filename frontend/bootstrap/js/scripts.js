b35d4dfa2cb75ebda41e6af7ea979aa4fef942c6
Ciaran Cruz <C21318233@mytudublin.ie>
2023-11-14 16:07:39 +0000

    account test page

--- frontend/bootstrap/js/scripts.js
+++ frontend/bootstrap/js/scripts.js
@@ -38,3 +38,20 @@ async function getapi() {
 
 
 
+function testFunction() {
+
+    var x = document.getElementById('accountPage');
+    var y = document.getElementById('loginPage');
+
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        y.style.display = "none";
+    }
+    else {
+        x.style.display = "none";
+        y.style.display = "block";
+    }
+}
+
+
+
