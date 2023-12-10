45adefbfae52d4756d99e642a3a6b39f961c0d94
ErnestDecina <ernestjohn007@gmail.com>
2023-12-10 12:25:59 +0000

    Commented AuthController.js

--- backend/src/api/controllers/AuthController.js
+++ backend/src/api/controllers/AuthController.js
@@ -10,16 +10,17 @@ class AuthController {
         try {
             const reuqest = req;
             
-            // Check if login failed
+            // Login user and check if it was unsuccessful
             if( !await AuthenticationService.login(reuqest) ) {
                 res.status(401).send({"auth": false});
                 return;
             }
                 
-
-
+            // Successful User Login
             res.status(200).send({"auth": true});
         } catch (error) {
+
+            // Check if Login code breaks on server side
             res.status(401)
         }
     } // End login()
@@ -31,12 +32,13 @@ class AuthController {
     ) {
         const reuqest = req;
 
+        // Log user out and check if it was unsuccessful
         if(! await AuthenticationService.logout(reuqest)) {
             res.status(401).send({"auth": false});
             return;
         }   
             
-
+        // Successful Logout 
         res.status(200).send({"auth": true});
     } // End logout()
 
@@ -45,12 +47,15 @@ class AuthController {
         req,
         res
     ) {
+
+        // Check if user is Session is valid and check if unsuccessful
         if(! await AuthenticationService.validateUser(req)) {
             res.status(401).send({"auth": false});
             return; 
         } // End if
             
 
+        // User Session is Valid
         res.status(200).send({"auth": true});
     } // End exmapleSessionCheck()
 } // End class AuthController
