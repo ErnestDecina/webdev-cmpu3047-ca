b3fddd9420261a61a667fa154bad93bb223c4f51
ErnestDecina <ernestjohn007@gmail.com>
2023-12-10 12:40:47 +0000

    Commented WorkoutController.js

--- backend/src/api/controllers/WorkoutController.js
+++ backend/src/api/controllers/WorkoutController.js
@@ -1,6 +1,3 @@
-import AccountsService from "../services/AccountsService.js";
-import AuthenticationService from "../services/AuthenticationService.js";
-import ExerciseService from "../services/ExerciseService.js";
 import WorkoutService from "../services/WorkoutService.js";
 
 
@@ -11,7 +8,11 @@ class WorkoutController {
         res
     ) {
         const request = req;
+
+        // Get Workouts
         const data = await WorkoutService.getWorkouts(request);
+
+        // Successful Get Workouts
         res.status(200).send(data);
     } // End getWorkouts
 
@@ -20,14 +21,18 @@ class WorkoutController {
         res
     ) {
         const request = req;
+
+        // Get Workout
         const data = await WorkoutService.getWorkout(request);
 
+        // Check if Workout was undefined
         if(!data) {
             res.status(401).send({
                 "name": "ERROR"
             });
         } // End if
 
+        // Successful Get Workout
         res.status(200).send(data);
     } // End getWorkouts
 
@@ -38,13 +43,13 @@ class WorkoutController {
     ) {
         const request = req;
 
+        // Create Workout and check if it was unsuccessful
         if(! await WorkoutService.createWorkout(request) ) {
             res.status(422).send({status: false});
             return;
         }
 
-
-
+        // Successful Create Workout
         res.status(200).send({status: true});
     } // End create Exercise
 
@@ -57,11 +62,13 @@ class WorkoutController {
     ) {
         const request = req;
 
+        // Delete Workout and check if it was unsuccessful
         if(! await WorkoutService.deleteWorkout(request)) {
             res.status(422).send({status: false});
             return;
         }
 
+        // Successful Delete Workout
         res.status(200).send({status: true});
     }
 
@@ -71,11 +78,13 @@ class WorkoutController {
     ) {
         const request = req;
         
+        // Update Workout and check if it was unsuccessful
         if(! await WorkoutService.updateWorkout(request)) {
             res.status(422).send({status: false});
             return;
         }
 
+        // Successful Update Workout
         res.status(200).send({status: true});
     }
 } // End class AuthController
